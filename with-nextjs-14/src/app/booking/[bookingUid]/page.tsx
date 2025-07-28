0834c5eba1698d3a9fb0a0aac065fe1c465a541d
CAZUK29 <RCARRARA@GMAIL.COM>
2025-07-29 00:36:27 +0100

    Add Netlify config for Next.js routing

--- with-nextjs-14/src/app/booking/[bookingUid]/page.tsx
+++
@@ -1,8 +0,0 @@
-import Booking from "../../../components/booking/BookingPage";
-
-export default async function BookingPage({ params }: { params: Promise<{ bookingUid: string }> }) {
-  const { bookingUid } = await params;
-  return (
-    <Booking bookingUid={bookingUid} />
-  )
-}
